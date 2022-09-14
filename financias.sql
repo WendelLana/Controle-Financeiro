e78b407b07f12292665ac91359bffdeccb3763df
Wendel Fernandes de Lana <48734976+WendelLana@users.noreply.github.com>
2022-09-14 20:40:49 -0300

    Add files via upload

---
+++ financias.sql
@@ -0,0 +1,25 @@
+-- TABLE
+CREATE TABLE Categoria (
+	categoria_id TEXT PRIMARY KEY,
+   	nome TEXT NOT NULL UNIQUE,
+	cor TEXT DEFAULT '1E1E1E',
+	icone TEXT
+);
+CREATE TABLE Gasto (
+	gasto_id TEXT PRIMARY KEY,
+   	data DATE DEFAULT (datetime('now','localtime')),
+	valor REAL NOT NULL,
+	descricao TEXT,
+ 	categoria_id TEXT,
+  	FOREIGN KEY (categoria_id) 
+      REFERENCES Categoria (categoria_id) 
+         ON DELETE NO ACTION 
+         ON UPDATE CASCADE
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
